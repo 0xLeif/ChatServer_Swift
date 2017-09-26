208c39239d296fb46c4829c076796b334ccfcd65
Zach Eriksen <zmeriksen@gmail.com>
2017-09-25 20:09:30 -0500

    Added correct dependencies

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-08-28 18:48:27 +0000
+// Date: 2017-09-26 01:01:28 +0000
 import PackageDescription
 let package = Package(
 	name: "Perfect-JSON-API",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+		.Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", majorVersion: 1),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2)
 	]
 )
