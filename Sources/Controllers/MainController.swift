6220ab15b8000142497b37f4aa93abd4b35a751e
ambid17 <sbambrose@unomaha.edu>
2017-10-17 20:38:18 -0500

    LoginAndRoomView

--- Sources/Controllers/MainController.swift
+++ Sources/Controllers/MainController.swift
@@ -16,19 +16,21 @@ final class MainController {
     
     var routes: [Route] {
         return [
-			Route(method: .get, uri: "/in", handler: loginView),
-            Route(method: .get, uri: "/", handler: indexView),
-            Route(method: .post, uri: "/", handler: addItem),
+            Route(method: .get, uri: "/rooms?handle={handle}&roomID={room}", handler: indexView),
+            Route(method: .get, uri: "/login", handler: redirectView),
+            Route(method: .post, uri: "/login", handler: addItem),
+            Route(method: .get, uri: "/home/handle/{handle}", handler: indexView),
         ]
     }
-	
+	func redirectView(request: HTTPRequest, response: HTTPResponse) {
+        mustacheRequest(request: request, response: response, handler: MustacheHelper(values: [:]), templatePath: request.documentRoot + "/signinup.mustache")
+    }
 	func loginView(request: HTTPRequest, response: HTTPResponse) {
 		do {
-			
-			var values = MustacheEvaluationContext.MapType()
+            var values = MustacheEvaluationContext.MapType()
 			values["rooms"] = try RoomAPI.allAsDictionary()
 			mustacheRequest(request: request, response: response, handler: MustacheHelper(values: values), templatePath: request.documentRoot + "/signinup.mustache")
-			
+
 		} catch {
 			response.setBody(string: "Error handling request: \(error)")
 				.completed(status: .internalServerError)
@@ -37,9 +39,13 @@ final class MainController {
     
     func indexView(request: HTTPRequest, response: HTTPResponse) {
         do {
-            
+            guard let handle = String(request.urlVariables["handle"]!) else {
+                response.completed(status: .badRequest)
+                return
+            }
             var values = MustacheEvaluationContext.MapType()
             values["rooms"] = try RoomAPI.allAsDictionary()
+            values["handle"] = handle
             mustacheRequest(request: request, response: response, handler: MustacheHelper(values: values), templatePath: request.documentRoot + "/chathome.mustache")
             
         } catch {
@@ -54,9 +60,9 @@ final class MainController {
                     response.completed(status: .badRequest)
                     return
             }
-//            _ = try RoomAPI.newRoom(withName: "Free", admin: User.user(withHandle: "leif").handle, users: [], messages: [])
+//            _ = try RoomAPI.newRoom(withName: handle, admin: User.user(withHandle: "leif").handle, users: [], messages: [])
             _ = try UserAPI.newUser(withHandle: handle, rooms: [], friends: [])
-            response.setHeader(.location, value: "/")
+            response.setHeader(.location, value: "/home/handle/\(handle)")
                 .completed(status: .movedPermanently)
         } catch {
             response.setBody(string: "Error handling request: \(error)")
@@ -70,10 +76,6 @@ final class MainController {
                 response.completed(status: .badRequest)
                 return
             }
-            try UserAPI.delete(handle: handle)
-            
-            response.setHeader(.location, value: "/ga")
-                .completed(status: .movedPermanently)
             
         } catch {
             response.setBody(string: "Error handling request: \(error)")
